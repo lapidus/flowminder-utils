557f1c27ebec8460894c6eeda9eef940a9cae477
zimrick <r.zimerman33@gmail.com>
2015-05-04 16:53:37 +0200

    Addded basic directive with map admin 2

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,10 @@
 'use strict';
 
-angular.module('flowminderUtils', [])
+angular.module('flowminderUtils', []);
+
+angular.module('flowminderUtils')
 	.controller('mainCtrl', ['$scope', function($scope) {
 
 		console.log('Main controller loaded.');
 
-	}])
-	.directive('map', [function() {
-
-		var linker = function(scope, element, attrs) {
-			console.log('Map directive loaded.');
-		};
-
-		return {
-			name: 'map',
-			scope: {
-				'map': '='
-			},
-			restrict: 'A',
-			template: '<h1>Map directive</h1>',
-			// templateUrl: '',
-			link: linker
-		};
 	}]);
