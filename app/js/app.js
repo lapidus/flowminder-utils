012ca5bcd49bb0a3081b58fd15d0d85b01060295
zimrick <r.zimerman33@gmail.com>
2015-05-04 17:46:08 +0200

    Set up promises in controller

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,35 @@
 angular.module('flowminderUtils', []);
 
 angular.module('flowminderUtils')
-	.controller('mainCtrl', ['$scope', function($scope) {
+	.controller('mainCtrl', ['$scope', '$http', '$q', function($scope, $http, $q) {
 
 		console.log('Main controller loaded.');
+
+		$scope.model = {};
+
+		var deferredMap = $q.defer();
+
+		$http.get('data/admin3/features.json')
+			.success(deferredMap.resolve)
+			.error(deferredMap.reject);
+
+		$scope.mapPromise = deferredMap.promise;
+
+		$scope.mapPromise.then(function(m) {
+			$scope.model.map = m;
+		});
+
+
+		var deferredData = $q.defer();
+
+		$http.get('data/data.csv')
+			.success(deferredData.resolve)
+			.error(deferredData.reject);
+
+		$scope.dataPromise = deferredData.promise;
+
+		$scope.dataPromise.then(function(d) {
+			$scope.model.data = d;
+		});
 
 	}]);
